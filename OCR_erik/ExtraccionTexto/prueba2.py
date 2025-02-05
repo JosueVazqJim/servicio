028b6c251b9ffda090ce895ac1a22a388ef78260
JosueVJ <josuevj.1e.49@gmail.com>
2025-02-05 00:32:53 -0600

    vector de antecedentes familiares y de clasificacion

--- OCR_erik/ExtraccionTexto/prueba2.py
+++ OCR_erik/ExtraccionTexto/prueba2.py
@@ -118,7 +118,7 @@ def verificar_texto(texto, image):
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Crear archivo txt
-my_file = open('D:\DOCUMENTOS\VirtualEnvPy\dataScience\source\Servicio\OCR_erik\ExtraccionTexto\TextoExtraido6.txt', 'w', encoding='utf-8')
+my_file = open('D:\DOCUMENTOS\VirtualEnvPy\dataScience\source\Servicio\OCR_erik\ExtraccionTexto\TextoExtraido10.txt', 'w', encoding='utf-8')
 
 # Leer la imagen con OpenCV
 image = cv2.imread(r'D:\DOCUMENTOS\VirtualEnvPy\dataScience\source\Servicio\OCR_erik\ExtraccionImagenes\cuadros\Zoom Reunion 2023-08-29 07-46-55\frame15.jpg')
